d60900604cad869c7eedb4593b6f618676b3e261
mobin-ux <mobin.gh505@gmail.com>
2021-07-20 21:36:12 +0430

    update

--- src/Components/Navigation/List.jsx
+++ src/Components/Navigation/List.jsx
@@ -50,18 +50,6 @@ const items = [
     isSvg: true,
   },
   {
-    title: "Info/State",
-    image: info,
-    url: "/",
-    isSvg: false,
-  },
-  {
-    title: "Contact",
-    image: contact,
-    url: "/",
-    isSvg: false,
-  },
-  {
     title: "Farms",
     image: farm,
     url: "/farm",
@@ -70,6 +58,18 @@ const items = [
   {
     title: "Blog",
     image: blog,
+    url: "/",
+    isSvg: false,
+  },
+  {
+    title: "Info/State",
+    image: info,
+    url: "/",
+    isSvg: false,
+  },
+  {
+    title: "Contact",
+    image: contact,
     url: "/",
     isSvg: false,
   },
@@ -218,7 +218,7 @@ const LastItem = (props) => {
           <path d="M8.71005 11.71L11.3001 14.3C11.6901 14.69 12.3201 14.69 12.7101 14.3L15.3001 11.71C15.9301 11.08 15.4801 10 14.5901 10H9.41005C8.52005 10 8.08005 11.08 8.71005 11.71Z"></path>
         </svg>
       </div>
-      <SubItem className="item"  show={showSubMenu}>
+      <SubItem className="item" show={showSubMenu}>
         <Link onClick={props.click} to="/">NFT</Link>
         <Link onClick={props.click} to="/">Loan</Link>
         <Link onClick={props.click} to="/">Governance</Link>
